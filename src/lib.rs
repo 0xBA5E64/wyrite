92b4c1267661abd185e67bc945fab34685c98a25
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2025-10-13 19:10:01 +0200

    fix: Enumerate `Option<time::OffsetDateTime>` properly!!!

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub struct Post {
     #[serde(with = "time::serde::rfc3339")]
     pub date_created: time::OffsetDateTime,
     // TODO: Serialize this too with time::serde::rfc3339, tricky since Option :|
+    #[serde(with = "time::serde::rfc3339::option")]
     pub date_published: Option<time::OffsetDateTime>,
 }
 
