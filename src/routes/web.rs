4603fa674d6b00395547a55b0ebe2c8aeeb8b2d4
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2026-07-23 21:40:02 +0200

    feat: Add minimally *functioning* web pages

--- src/routes/web.rs
+++ src/routes/web.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
-use axum::extract::State;
-use axum::response::IntoResponse;
+use axum::extract::{Path, State};
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use serde_json::json;
 use wyrite::AppState;
@@ -9,6 +9,7 @@ use wyrite::AppState;
 pub fn get_routes() -> axum::Router<Arc<AppState>> {
     axum::Router::new()
         .route("/", get(view_home))
+        .route("/post/{slug}", get(view_post))
         .route("/posts", get(view_posts))
 }
 
@@ -19,15 +20,37 @@ async fn view_home(app_state: State<Arc<AppState>>) -> impl IntoResponse {
         "body": "Welcome to wyrite"
     });
 
-    app_state.templates.render("index", data).unwrap();
+    Html(app_state.templates.render("index", data).unwrap())
+}
+
+#[axum::debug_handler]
+async fn view_post(app_state: State<Arc<AppState>>, Path(slug): Path<String>) -> impl IntoResponse {
+    let post = sqlx::query_as!(
+        wyrite::Post,
+        "SELECT * FROM post_view WHERE \"slug!\" = $1",
+        slug
+    )
+    .fetch_optional(&app_state.db_pool)
+    .await
+    .unwrap();
+
+    let data = &json!({
+        "post": post
+    });
+
+    Html(app_state.templates.render("post", data).unwrap())
 }
 
 #[axum::debug_handler]
 async fn view_posts(app_state: State<Arc<AppState>>) -> impl IntoResponse {
+    let posts = sqlx::query_as!(wyrite::Post, "SELECT * FROM post_view")
+        .fetch_all(&app_state.db_pool)
+        .await
+        .unwrap();
+
     let data = &json!({
-        "title": "Index Page",
-        "body": "Not yet ready"
+        "posts": posts
     });
 
-    app_state.templates.render("index", data).unwrap();
+    Html(app_state.templates.render("posts", data).unwrap())
 }
